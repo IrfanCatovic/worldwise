91fdb085d8d4ca430eaaacd5aa1060c6572ed205
IrfanCatovic <catovicc84@gmail.com>
2024-01-28 16:58:57 +0100

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./Pages/Product";
+import Pricing from "./Pages/Pricing";
+import Homepage from "./Pages/Homepage";
+
 function App() {
-  return <div>worldWise</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="home" element={<Homepage />} />
+          <Route path="product" elemet={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
