71ba92f4c9b6c637078d6bf25cf46fac5150dff8
IrfanCatovic <catovicc84@gmail.com>
2024-01-31 16:14:36 +0100

    css

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 function Homepage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h1>HOMEPAGE</h1>
       {/* <a href="/pricing">Pricing</a> old way to rout */}
+      <Link to="/app">Go to the App</Link>
     </div>
   );
 }
