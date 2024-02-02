89f63138f9d3d7ecf8c3be5395658a09399da95a
IrfanCatovic <catovicc84@gmail.com>
2024-02-02 20:32:07 +0100

    Finished navigation and some pages

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,9 +1,11 @@
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
+import Logo from "./Logo";
 
 function PageNav() {
   return (
     <nav className={styles.nav}>
+      <Logo />
       <ul>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
@@ -12,7 +14,9 @@ function PageNav() {
           <NavLink to="/product">Product</NavLink>
         </li>
         <li>
-          <NavLink to="/login">Login</NavLink>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
