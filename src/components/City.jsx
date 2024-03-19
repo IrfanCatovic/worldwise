c3a62878dac5d6418b37f94f75bd96d9b41bb58e
IrfanCatovic <catovicc84@gmail.com>
2024-03-19 02:27:59 +0100

    ContextAPI

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
+import { useEffect, useState } from "react";
+import { useCitis } from "../contexts/CitiesContext";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -12,67 +14,75 @@ const formatDate = (date) =>
 
 function City() {
   const { id } = useParams();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat"); //dohvatimo iz URL
-  const lng = searchParams.get("lng"); //dohvati iz URL
+
+  const { getCity, currentCity } = useCitis();
+
+  useEffect(
+    function () {
+      getCity(id);
+    },
+    [id]
+  );
+
+  // const [searchParams, setSearchParams] = useSearchParams();
+  // const lat = searchParams.get("lat"); //dohvatimo iz URL
+  // const lng = searchParams.get("lng"); //dohvati iz URL
 
   //ovde dobijamo u ovo slucaju objekat oblika id:broj
   //zato sto smo u app kreirali putanju "cities/:id"
   // TEMP DATA
-  const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
-  };
+  // const currentCity = {
+  //   cityName: "Lisbon",
+  //   emoji: "🇵🇹",
+  //   date: "2027-10-31T15:59:59.138Z",
+  //   notes: "My favorite city so far!",
+  // };
+
+  // return (
+  //   <>
+  //     <h1>CITY {id}</h1>
+  //     <p>
+  //       Position: {lat}, {lng}
+  //     </p>
+  //   </>
+  // );
 
   const { cityName, emoji, date, notes } = currentCity;
-
   return (
-    <>
-      <h1>CITY {id}</h1>
-      <p>
-        Position: {lat}, {lng}
-      </p>
-    </>
+    <div className={styles.city}>
+      <div className={styles.row}>
+        <h6>City name</h6>
+        <h3>
+          <span>{emoji}</span> {cityName}
+        </h3>
+      </div>
+
+      <div className={styles.row}>
+        <h6>You went to {cityName} on</h6>
+        <p>{formatDate(date || null)}</p>
+      </div>
+
+      {notes && (
+        <div className={styles.row}>
+          <h6>Your notes</h6>
+          <p>{notes}</p>
+        </div>
+      )}
+
+      <div className={styles.row}>
+        <h6>Learn more</h6>
+        <a
+          href={`https://en.wikipedia.org/wiki/${cityName}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Check out {cityName} on Wikipedia &rarr;
+        </a>
+      </div>
+
+      <div>{/* <ButtonBack /> */}</div>
+    </div>
   );
-
-  // return;
-  //   <div className={styles.city}>
-  //     <div className={styles.row}>
-  //       <h6>City name</h6>
-  //       <h3>
-  //         <span>{emoji}</span> {cityName}
-  //       </h3>
-  //     </div>
-
-  //     <div className={styles.row}>
-  //       <h6>You went to {cityName} on</h6>
-  //       <p>{formatDate(date || null)}</p>
-  //     </div>
-
-  //     {notes && (
-  //       <div className={styles.row}>
-  //         <h6>Your notes</h6>
-  //         <p>{notes}</p>
-  //       </div>
-  //     )}
-
-  //     <div className={styles.row}>
-  //       <h6>Learn more</h6>
-  //       <a
-  //         href={`https://en.wikipedia.org/wiki/${cityName}`}
-  //         target="_blank"
-  //         rel="noreferrer"
-  //       >
-  //         Check out {cityName} on Wikipedia &rarr;
-  //       </a>
-  //     </div>
-
-  //     <div>
-  //       <ButtonBack />
-  //     </div>
-  //   </div>
 }
 
 export default City;
